d8d4c568531fee37c5c2b1c3b859e6991598be7f
PraneethJain <praneethjain005@gmail.com>
2024-01-22 19:04:54 +0530

    feat: home page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NavBar from "./components/navbar";
+import Footer from "./components/footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header>header</header>
+        <NavBar />
         {children}
-        <footer>footer</footer>
+        <Footer />
       </body>
     </html>
   );
