8dac6486ff57418b1b264d638e6069425ea565c0
PraneethJain <praneethjain005@gmail.com>
2024-01-22 17:36:06 +0530

    feat: frontend project structure

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "E-Summit 2024",
+  description: "By E-Cell",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <header>header</header>
+        {children}
+        <footer>footer</footer>
+      </body>
+    </html>
+  );
+}
