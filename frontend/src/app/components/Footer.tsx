40a15d53988869a22a9e09aeea42303015507584
PraneethJain <praneethjain005@gmail.com>
2024-01-24 00:49:01 +0530

    misc: capitalize component names

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -1,4 +1,4 @@
-import "./footer.css";
+import "./Footer.css";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import XIcon from '@mui/icons-material/X';
