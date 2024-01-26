526ee99110b2a82100d71be967a74eba1e70f4e3
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-01-26 23:45:06 +0530

    fix: responsive navbar

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import "./Navbar.css";
 import Image from "next/image";
 import esummitLogo from "@/assets/icon_homepage.svg";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <header className="navbar">
+    <header className={`navbar ${menuOpen ? "menu-open" : ""}`}>
       <div className="left">
         <a href="#">
           <Image
@@ -15,8 +24,11 @@ export default function NavBar() {
           />
         </a>
       </div>
-      <nav className="right">
-        <ul>
+      <nav className={`right ${menuOpen ? "menu-open" : ""}`}>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <MenuIcon style={{ fontSize: 36 }} />
+        </div>
+        <ul onClick={toggleMenu}>
           <li>
             <a href="#">Home</a>
           </li>
